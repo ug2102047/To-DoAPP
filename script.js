2e3a65d10fd77bc010a2809d8e0942aff9b67aa9
Puspita Baidya <ug2102047@cse.pstu.ac.bd>
2026-05-25 23:23:14 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+const inputBox = document.getElementById("input-box");
+const addBtn = document.getElementById("add-btn");
+const list = document.getElementById("list");
+
+function addTask() {
+    if (inputBox.value.trim() === "") {
+        alert("Please enter a task.");
+        return;
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        list.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";   
+    saveData();
+}
+addBtn.addEventListener("click", addTask);
+
+inputBox.addEventListener("keydown", function(e) {
+    if (e.key === "Enter") {
+        addTask();
+    }
+});
+list.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+    }
+    else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        saveData();
+    }
+}, false);
+
+function saveData() {
+    localStorage.setItem("data", list.innerHTML);
+}
+
+function showTask(){
+    list.innerHTML = localStorage.getItem("data");
+}
+showTask();
